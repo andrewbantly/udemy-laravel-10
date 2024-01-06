7e82bfa730683739295c3788ab40453f66b29ce2
andrewbantly <andrewbantly@gmail.com>
2024-01-05 18:10:07 -0800

    updated form validation with TaskRequest Class

---
+++ l10-task-list/resources/views/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('content')
+<style>
+    .error-message {
+      color: red;
+    }
+  </style>
+<!-- {{ $errors }} -->
+<form method='POST' action="{{route('tasks.store')}}">
+    @csrf
+    <div>
+        <label for='title'>Title</label>
+        <input text="text" name="title" id="title" value="{{ old('title')}}"/>
+        @error('title')
+        <p class="error-message">{{ $message }}</p>
+      @enderror
+    </div>
+    <div>
+        <label for='description'>Description</label>
+        <textarea name="description" id="description" rows="5" value="{{ old('description')}}"></textarea>
+        @error('description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+    </div>
+    <div>
+        <label for='long_description'>Long Description</label>
+        <textarea name="long_description" id="long_description" rows="10" value="{{ old('long_description')}}"></textarea>
+        @error('long_description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+    </div>
+    <button type="submit">Add Task</button>
+</form>
+@endsection
--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -7,7 +7,7 @@
 
         @foreach ($tasks as $task)
         <div>
-            <a href="{{route('tasks.show', ['id' => $task->id])}}">{{$task->title}}</a>
+            <a href="{{route('tasks.show', ['task' => $task->id])}}">{{$task->title}}</a>
         </div>
         @endforeach
     
