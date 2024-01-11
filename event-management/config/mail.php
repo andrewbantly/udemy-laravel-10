83c00fabc08580e26040f962501ec55a0e220b8b
andrewbantly <andrewbantly@gmail.com>
2024-01-11 11:52:35 -0800

    event email notifications

--- event-management/config/mail.php
+++ event-management/config/mail.php
@@ -34,6 +34,14 @@ return [
     */
 
     'mailers' => [
+        'mailhog' => [
+            'transport' => 'smtp',
+            'host' => '127.0.0.1',     // MailHog SMTP server host
+            'port' => 1025,            // MailHog SMTP server port
+            'encryption' => null,      // No encryption is used for MailHog
+            'username' => null,        // No username is required for MailHog
+            'password' => null,        // No password is required for MailHog
+        ],
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
